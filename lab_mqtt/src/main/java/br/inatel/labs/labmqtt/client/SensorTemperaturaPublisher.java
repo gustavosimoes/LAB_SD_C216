11341887c7617d43fb9e57ff6ab7a763134e3db0
Gustavo Teixeira Simões <64706991+gustavosimoes@users.noreply.github.com>
2022-12-03 08:25:51 -0300

    Add files via upload

---
+++ lab_mqtt/src/main/java/br/inatel/labs/labmqtt/client/SensorTemperaturaPublisher.java
@@ -0,0 +1,35 @@
+package br.inatel.labs.labmqtt.client;
+
+import org.eclipse.paho.client.mqttv3.*;
+
+import java.util.Random;
+import java.util.UUID;
+
+public class SensorTemperaturaPublisher {
+    public static void main(String[] args) throws MqttException {
+
+        String publisherId = UUID.randomUUID().toString();
+        IMqttClient publisher = new MqttClient(MyConstants.URI_BROKER, publisherId);
+
+        MqttMessage msg = getTemperaturaSolo();
+        msg.setQos(0);
+        msg.setRetained(true);
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+        publisher.connect(options);
+
+        publisher.publish(MyConstants.TOPIC_1, msg);
+
+        publisher.disconnect();
+    }
+
+    private static MqttMessage getTemperaturaSolo() {
+        Random r = new Random();
+        double temperatura = 80 + r.nextDouble() * 20.0;
+        byte[] payload = String.format("T:%04.2f", temperatura).getBytes();
+        return new MqttMessage(payload);
+    }
+}
